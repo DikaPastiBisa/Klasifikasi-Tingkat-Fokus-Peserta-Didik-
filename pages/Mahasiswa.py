cf3f5af195c3f492a3f1a235d1c3efde6817aa1f
Dikaprasetya <136421914+DikaPastiBisa@users.noreply.github.com>
2026-06-30 19:23:49 +0700

    Update Mahasiswa.py

--- pages/Mahasiswa.py
+++ pages/Mahasiswa.py
@@ -399,37 +399,29 @@ if camera_mode == "Web Browser":
 
                     {
                         "urls": [
-                            "stun:stun.relay.metered.ca:80",
+                            "stun:stun.l.google.com:19302",
+                            "stun:stun1.l.google.com:19302",
+                            "stun:stun2.l.google.com:19302",
+                            "stun:stun3.l.google.com:19302",
+                            "stun:stun4.l.google.com:19302",
                         ]
                     },
+
                     {
                         "urls": [
-                            "turn:standard.relay.metered.ca:80",
-                        ],
-                        "username": "320099d3c6d3df05a5e4ff74A",
-                        "credential": "32EAr24ItPARmp2WA",
+                            "stun:openrelay.metered.ca:80"
+                        ]
                     },
+
                     {
                         "urls": [
-                            "turn:standard.relay.metered.ca:80?transport=tcp",
+                            "turn:openrelay.metered.ca:80",
+                            "turn:openrelay.metered.ca:443",
+                            "turn:openrelay.metered.ca:443?transport=tcp"
                         ],
-                        "username": "320099d3c6d3df05a5e4ff74A",
-                        "credential": "32EAr24ItPARmp2WA",
-                    },
-                    {
-                        "urls": [
-                            "turn:standard.relay.metered.ca:443",
-                        ],
-                        "username": "320099d3c6d3df05a5e4ff74A",
-                        "credential": "32EAr24ItPARmp2WA",
-                    },
-                    {
-                        "urls": [
-                            "turns:standard.relay.metered.ca:443?transport=tcp",
-                        ],
-                        "username": "320099d3c6d3df05a5e4ff74A",
-                        "credential": "32EAr24ItPARmp2WA",
-                    },
+                        "username": "openrelayproject",
+                        "credential": "openrelayproject",
+                    }
 
                 ]
             },
